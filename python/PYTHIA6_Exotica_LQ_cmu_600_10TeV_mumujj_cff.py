e0d09c5081d92ccba248b79f3967125cc68810ce
ferencek <>
2009-09-23 19:56:25 +0000

    version for CMSSW_3_X_Y

--- python/PYTHIA6_Exotica_LQ_cmu_600_10TeV_mumujj_cff.py
+++ python/PYTHIA6_Exotica_LQ_cmu_600_10TeV_mumujj_cff.py
@@ -1,12 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
+source = cms.Source("EmptySource")
+
 from Configuration.GenProduction.PythiaUESettings_cfi import *
-source = cms.Source("PythiaSource",
+generator = cms.EDFilter("Pythia6GeneratorFilter",
     pythiaHepMCVerbosity = cms.untracked.bool(False),
     maxEventsToPrint = cms.untracked.int32(0),
     pythiaPylistVerbosity = cms.untracked.int32(0),
     filterEfficiency = cms.untracked.double(1.0),
-    comEnergy = cms.untracked.double(10000.0),
+    comEnergy = cms.double(10000.0),
     crossSection = cms.untracked.double(0.03220),
     PythiaParameters = cms.PSet(
         pythiaUESettingsBlock,
@@ -26,7 +28,9 @@ source = cms.Source("PythiaSource",
 )
 
 configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.2 $'),
+        version = cms.untracked.string('$Revision: 1.3 $'),
         name = cms.untracked.string('$Source: /cvs_server/repositories/CMSSW/UserCode/Leptoquarks/LQGenFilter/python/PYTHIA6_Exotica_LQ_cmu_600_10TeV_mumujj_cff.py,v $'),
         annotation = cms.untracked.string('default documentation string for PYTHIA6_Exotica_LQ_cmu_600_10TeV_mumujj_cff.py')
 )
+
+ProductionFilterSequence = cms.Sequence(generator)
